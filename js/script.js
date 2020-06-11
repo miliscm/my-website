d6a814226e4d10c3c848d6c9e450b16cf08d882e
Milena Mocci <miliscm@gmail.com>
2020-06-11 14:53:21 -0300

    alteração no linkdin

---
+++ js/script.js
@@ -0,0 +1,10 @@
+new SimpleForm({
+  form: ".formphp", // seletor do formulário
+  button: "#enviar", // seletor do botão
+  erro:
+    "<div id='form-erro'><p>Um erro ocorreu, tente para o email milena.mocci@micalimo.com.br.</p></div>", // mensagem de erro
+  sucesso:
+    "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
+});
+
+new SimpleAnime();
